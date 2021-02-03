9b50cc914963d3ccb53d2ee68730cb37a0725731
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2021-02-03 22:37:14 +0200

    Create Subject.scm

---
+++ Subject.scm
@@ -0,0 +1,11 @@
+(string=? "form and deform is rather personal issue" "form and deform is rather personal issue")  ;; => #t
+(string=? "senses form us" "senses form us");; => #t
+(string=? "art forms us" "art forms us")                    ;; => #t
+(string=? "ancient greek were close to industrial revolution" "ancient greek were close to industrial revolution")     
+(string=? "we struggle much on this idea"  "we struggle much on this idea")
+(string=? "there is no difference in our enviroment unless we do the difference"  "there is no difference in our enviroment unless we do the difference") 
+(string=? "there is a pluralism of colours movements ideas and sounds"  "there is a pluralism of colours movements ideas and sounds") 
+(string=? "we can't overcome history unless we built it"  "we can't overcome history unless we built it")
+(string=? "Building history is reimagining it"  "Building history is reimagining it") 
+(string=? "If the same story repeats we need to reimagine a different plot" "If the same story repeats we need to reimagine a different plot")
+(string=? "there is no beginning or end it's a subject into another subject" "there is no beginning or end it's a subject into another subject")

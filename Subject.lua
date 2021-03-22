9edec899bdba4664fc136dbba556d913fa130fe5
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2021-03-22 07:27:14 +0200

    Update Subject.lua

--- Subject.lua
+++ Subject.lua
@@ -1,4 +1,4 @@
 
-for i=1, 1000000000000000000000000000000000000 do
-	print('In the future everyone will be famous for fifteen minutes')
+1000000000000000000000.times do
+puts "Death is a prejudice for Gods"
 end
